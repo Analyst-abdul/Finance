78c6c53abd5bf4eb64af7e51d9238898338e2c32
Analyst-abdul <162406280+Analyst-abdul@users.noreply.github.com>
2025-10-26 14:51:05 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,7 @@
+// js/config.js
+// Keep this file private in your repo (but it's fine for personal projects).
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbx_d3WcSW232by02_lBTZhTZJRHIj88df0si5U-UZsCoDQbL5ar5TjvZ1KJEvLuPF4/exec",
+  API_KEY: "abdul8906692526balikejas",
+  CURRENCY_SYMBOL: "₹"
+};
